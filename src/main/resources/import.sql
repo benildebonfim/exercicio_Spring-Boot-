5c3d0abcd7ea49c3e667c5c107c1b902f0d1480f
benildebonfim <benildeconceicaobonfim@gmail.com>
2022-04-11 13:06:20 +0000

    web service
    Exercicio(web service)

---
+++ src/main/resources/import.sql
@@ -0,0 +1,9 @@
+INSERT INTO tb_department(name) VALUES ('Gestão');
+INSERT INTO tb_department(name) VALUES ('Informática');
+INSERT INTO tb_department(name) VALUES ('Matemática');
+
+INSERT INTO tb_user(department_id, name, email) VALUES (2,'Benilde', 'benildeconceicaobonfim@gmail.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (3,'Ana', 'anadias@gmail.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (1,'ailton', 'ailtonbonfim@gmail.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (2,'Denis', 'denisquintas@gmail.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (2,'Watson', 'watsonsantos@gmail.com');
